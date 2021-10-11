77e839dcfcba61f3de76917eec22d8bbe9de69f0
Aqua Minato <sweet4182691@gmail.com>
2021-10-11 18:35:02 +0900

    ogp image 追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,14 +18,14 @@ export default class MyDocument extends Document {
           />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://holo-tube.vercel.app/" />
-          <meta property="og:image" content="" />
+          <meta property="og:image" content="./public/holodata.png" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="HoloData" />
           <meta
             name="twitter:description"
             content="ホロライブの情報閲覧サイト"
           />
-          <meta name="twitter:image" content="" />
+          <meta name="twitter:image" content="./public/holodata.png" />
           <meta name="twitter:site" content="@arisahyper" />
           <meta name="twitter:creator" content="@arisahyper" />
         </Head>
