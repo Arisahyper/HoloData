14402042c166669d0778d37e91f249a069d0ac5a
Aqua Minato <sweet4182691@gmail.com>
2021-10-11 11:45:09 +0900

    twitterのfotterのレイアウト崩れを修正

--- pages/twitter.tsx
+++ pages/twitter.tsx
@@ -17,20 +17,23 @@ const Twitter = () => {
 
   return (
     <>
-      <div style={{ height: "100%", position: "relative", bottom: "3rem" }}>
-        <DemoSideBar />
-        <div style={{ textAlign: "center" }}>
+      <div style={{ height: "100%" }}>
+          <DemoSideBar />
+        <div style={{ height:"100vh",textAlign: "center", position: "relative", bottom: "3rem"  }}>
           <a
             className="twitter-timeline"
             data-width="80vh"
-            data-height="80vh"
+            data-height="97vh"
+            // data-theme="dark"
             data-theme="light"
             data-chrome="noheadernofooternoborders"
             href="https://twitter.com/hololivetv/lists/list6?ref_src=twsrc%5Etfw"
-          ></a>
+          />
+          <div style={{ width:"100%", position:"fixed", bottom: 0 }}>
+            <Footer />
+          </div>
         </div>
       </div>
-      <Footer />
     </>
   );
 };
